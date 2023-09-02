1bd34b90d1a9266bf21a9a069b01311bdf6207bf
aronglee <arong16@naver.com>
2023-09-02 19:08:44 +0900

    Feat : 상품 등록 추가

---
+++ src/api/upload.ts
@@ -0,0 +1,40 @@
+import instance from './axios';
+
+interface product {
+  title: string;
+  price: string;
+  category: string;
+  explanation: string;
+  option: string;
+}
+
+export async function upload(file: File, product: product) {
+  const formData = new FormData();
+
+  formData.append('file', file);
+  formData.append('title', product.title);
+  formData.append('price', product.price);
+  formData.append('category', product.category);
+  formData.append('explanation', product.explanation);
+  formData.append('option', product.option);
+
+  window.console.log(product.title);
+  window.console.log(file);
+  window.console.log(product.price);
+  // FormData의 key 확인
+  for (const key of formData.keys()) {
+    window.console.log(key);
+  }
+  // FormData의 value 확인
+  for (const value of formData.values()) {
+    window.console.log(value);
+  }
+
+  await instance
+    .post('/products', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then((res) => window.console.log(res));
+}
--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,6 +1,127 @@
-import React from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
+import { upload } from '../api/upload';
+import swal from 'sweetalert';
 
-export default function AddProduct() {
-  return <div>AddProduct!</div>;
+interface product {
+  title: string;
+  price: string;
+  category: string;
+  explanation: string;
+  option: string;
 }
+export default function AddProduct() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [product, setProduct] = useState<product>({
+    title: '',
+    price: '',
+    category: '',
+    explanation: '',
+    option: '',
+  });
+  const [file, setFile] = useState<File | null>(null);
+  const fileInput = useRef<HTMLInputElement>(null);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      if (files && files.length > 0) {
+        setFile(files[0]);
+        window.console.log(files);
+        window.console.log(files[0]);
+      }
+      return;
+    }
+    setProduct((product) => ({ ...product, [name]: value }));
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+    if (!file) {
+      return;
+    }
+    upload(file, product).then(() => {
+      swal('⭐️ 제품이 등록되었습니다.');
+      setIsLoading(false);
+      setFile(null);
+      setProduct({ title: '', price: '', category: '', explanation: '', option: '' });
+      if (fileInput.current) {
+        fileInput.current.value = '';
+      }
+    });
+  };
+
+  return (
+    <div className="flex flex-col my-5 w-7/12 mx-auto">
+      <h1 className="text-center font-googleRoboto font-semibold text-xl">
+        Register a new product
+      </h1>
+
+      {file && (
+        <img src={URL.createObjectURL(file)} className="max-h-[35rem] mt-5" alt="local file" />
+      )}
+      <form className="flex flex-col " onSubmit={handleSubmit}>
+        <input
+          type="file"
+          onChange={handleChange}
+          accept="image/*"
+          name="file"
+          ref={fileInput}
+          required
+          className="border border-gray-300 p-3 mt-5 mb-1.5"
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ''}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 p-3 my-1.5"
+          placeholder="제품명"
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? 0}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 p-3 my-1.5"
+          placeholder="가격"
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ''}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 p-3 my-1.5"
+          placeholder="카테고리"
+        />
+        <input
+          type="text"
+          name="explanation"
+          value={product.explanation ?? ''}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 p-3 my-1.5"
+          placeholder="제품 설명"
+        />
+        <input
+          type="text"
+          name="option"
+          value={product.option ?? ''}
+          onChange={handleChange}
+          required
+          className="border border-gray-300 p-3 my-1.5"
+          placeholder="옵션들(콤마(,)로 구분)"
+        />
+        <button
+          disabled={isLoading}
+          className="border border-black bg-black text-white font-googleRoboto p-3 my-1.5"
+        >
+          {isLoading ? 'Uploading...' : 'Register Product'}
+        </button>
+      </form>
+    </div>
+  );
+}
