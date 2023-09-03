e94c438fd61339245840eb4554f6abfa2c3c36fb
aronglee <arong16@naver.com>
2023-09-04 00:33:27 +0900

    Style : font 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
+    fontFamily: {
+      googleOswald: ['googleOswald'],
+      googleRoboto: ['googleRoboto'],
+      nanumSquareNeoL: ['nanumSquareNeoL'],
+      nanumSquareNeoR: ['nanumSquareNeoR'],
+    },
   },
   plugins: [],
 };
