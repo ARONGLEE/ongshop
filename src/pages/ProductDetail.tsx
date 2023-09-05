26506afe4dd0dcc12d5055401275961d2b88bf51
aronglee <arong16@naver.com>
2023-09-06 04:20:38 +0900

    Feat : 상품 상세보기 추가

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { carts } from '../api/carts';
+import { useAuthContext } from '../context/AuthContext';
 
 export default function ProductDetail() {
-  return <div>ProductDetail!</div>;
-}
+  const { user } = useAuthContext();
+  const {
+    state: {
+      product: { no, imgUrl, explanation, option, price, title },
+    },
+  } = useLocation();
 
+  const [selected, setSelected] = useState(option && option[0]);
+  window.console.log(option);
+
+  const handleSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelected(e.target.value);
+  };
+  const handleClick = () => {
+    // (no키값을 productNo로 변경해서 보내기)
+    // const product = { no, memberNo: 1, option: selected, quantity: 1 };
+    carts(no, 1, selected, '1');
+  };
+
+  return (
+    <section className="flex flex-col md:flex-row p-4">
+      <img className="w-full max-w-[60%] px-4 basis-1/2" src={imgUrl} alt={title} />
+      <div className="w-full max-w-[40%] basis-1/2 flex flex-col p-4">
+        <h2 className="text-3xl font-bold py-2 font-nanumSquareNeoR">{title}</h2>
+        <p className="py-4 text-lg font-nanumSquareNeoL">{explanation}</p>
+        <div className="flex items-center pb-4">
+          <label htmlFor="select" className="pr-3 font-semibold font-nanumSquareNeoR">
+            옵션:
+          </label>
+          <select
+            className="flex-1 outline-none font-googleRoboto"
+            id="select"
+            onChange={handleSelect}
+            value={selected}
+          >
+            {option && option.map((size: string, idx: number) => <option key={idx}>{size}</option>)}
+          </select>
+        </div>
+        {/* {success && <p className="my-2">⭐️{success}</p>} */}
+        <button
+          className="border border-black bg-black text-white font-googleRoboto p-3 my-1.5"
+          onClick={handleClick}
+          disabled={user === null || undefined}
+        >
+          {`ADD \u00A0\u00A0\u00A0\u00A0 ₩${price}`}
+        </button>
+      </div>
+    </section>
+  );
+}
